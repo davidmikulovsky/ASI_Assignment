84ab1fd6da08398cb67ce92905f193bdf2a74b2f
dry950526@gmail.com <dry950526@gmail.com>
2018-10-16 13:33:30 +0300

    update links

--- data_analysis.py
+++ data_analysis.py
@@ -2,10 +2,10 @@ import sqlite3
 import requests
 
 
-def get_ips(db_path, ips, time_stamp):
+def get_ips(db_path, ips):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    cursor = c.execute("SELECT IP FROM pytomo_crawl_+")
+    cursor = c.execute("SELECT IP FROM pytomo_crawl_2018_10_16_04_41_53")
     for each in cursor:
         print(each)
         res = requests.get('http://ip-api.com/json/' + each[0])
@@ -13,6 +13,6 @@ def get_ips(db_path, ips, time_stamp):
         print(res.json())
 
 if __name__ == '__main__':
-    db_path = './databases/DingdeMacBook-Pro.local.youtube.2018-10-16.10_21_56.pytomo_database.db'
+    db_path = './databases/host.localdomain.youtube.2018-10-16.04_41_53.pytomo_database.db'
     ips = []
     get_ips(db_path, ips)
