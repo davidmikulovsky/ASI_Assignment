6cb1f7c6c563e10ad5c00ce74b55ee494b6b3ef7
dry950526@gmail.com <dry950526@gmail.com>
2018-10-18 15:03:17 +0300

    add ytb api

--- youtube.py
+++ youtube.py
@@ -1,11 +1,25 @@
+
+import requests
 import pytomo.start_pytomo as start_pytomo
 import pytomo.config_pytomo as config_pytomo
 
 
 
 config_pytomo.LOG_FILE = '-'
-file_name = 'test.txt'
+file_name = 'somefile.txt'
 
+def get_most_popular_video():
+    res = requests.get('https://www.googleapis.com/youtube/v3/videos?'
+                  'chart=mostPopular&'
+                  'key=AIzaSyAE5NngJQOE40hLqjRtIr0GP5j9BFFF-Kw&'
+                  'part=id&'
+                  'maxResults=50&'
+                       'regionCode=FI'
+                  )
+    most_pop_list = res.json()['items']
+    with open('somefile.txt', 'a') as the_file:
+        for each in most_pop_list:
+            the_file.write('https://www.youtube.com/watch?v=' + each['id']+'\n')
 
 ## log_file = start_pytomo.configure_log_file(timestamp)
 if __name__ == '__main__':
@@ -13,7 +27,7 @@ if __name__ == '__main__':
     config_pytomo.INPUT_FILE = file_name
     config_pytomo.BATCH_MODE = True
     start_pytomo.main()
-    print start_pytomo.timestamp
+    #get_most_popular_video()
 
 
 
